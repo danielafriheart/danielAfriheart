3bf416d6bb57d9b8cfe851d94d9145789c4084a6
Daniel Esuola <esuoladaniel002@gmail.com>
2022-05-08 13:47:57 +0100

    files

---
+++ pages/darkmode.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+//  }
+ const darkmodeOn = () =>{
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
