189bd332b1dc23dc4fd3f8dbb85cc02c225737f1
Daniel Esuola <esuoladaniel002@gmail.com>
2022-06-14 12:59:24 +0100

    updated website looks and feel, added a couple changes to the fonts too

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ window.addEventListener("click", () => {
 // *********************
 // This Code is for only the floating card in right bottom corner
 // **********************
-
 const touchButton = document.querySelector(".float-text");
 const card = document.querySelector(".float-card-info");
 const close = document.querySelector(".gg-close-r");
